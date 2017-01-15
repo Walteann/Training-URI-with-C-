9cf1d297cac0c38df2cdd242444ba9d20717013a
Walteann <walteann3@gmail.com>
2017-01-14 22:23:46 -0300

    Commit 1001

---
+++ URI Problem/URI Problem/beginner/Exer1001.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using System.Text;
+
+
+namespace URI_Problem
+{
+    class Exer1001
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X;
+            A = Convert.ToInt16(Console.ReadLine());
+            B = Convert.ToInt16(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("X = "+X);
+            Console.ReadKey();
+            
+        }
+    }
+}
