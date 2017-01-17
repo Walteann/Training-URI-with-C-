780fe7d873aabe8a6b436f370388374fc5552772
Walteann <walteann3@gmail.com>
2017-01-16 23:21:27 -0300

    commit 1003

---
+++ URI Problem/URI Problem/beginner/Exer1003.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_Problem.beginner
+{
+    class Exer1003
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int SOMA = A + B;
+            Console.WriteLine("SOMA = " + SOMA);
+            Console.ReadKey();
+        }
+    }
+}
